6f04e8c263e0acebaadd4eb7376dda349b7c8b93
Akindu Himan <akinduhiman2@gmail.com>
2025-10-28 00:21:05 +0530

    update Uvicorn run configuration to set log level to info and disable access logs

--- property-recommendation/main.py
+++ property-recommendation/main.py
@@ -219,4 +219,10 @@ def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8011)
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8011,
+        log_level="info",
+        access_log=False
+    )
