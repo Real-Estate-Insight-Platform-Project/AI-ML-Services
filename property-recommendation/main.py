1e3d743f88c75e7bdd8dbc097e545702608a5405
Akindu Himan <akinduhiman2@gmail.com>
2025-10-28 11:10:33 +0530

    update webserver base URL to use IP address and change Uvicorn port to 8011

--- property-recommendation/main.py
+++ property-recommendation/main.py
@@ -222,7 +222,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app,
         host="0.0.0.0",
-        port=7000,
+        port=8011,
         log_level="info",
         access_log=False
     )
